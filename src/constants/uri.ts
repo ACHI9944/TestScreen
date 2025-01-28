5ec0e95ca01d97e1a3c18fa32ba61347da010d39
achi <achi9944@gmail.com>
2025-01-28 14:38:39 +0400

    added tabBarIcon component

---
+++ src/constants/uri.ts
@@ -0,0 +1,4 @@
+export const homeUri =
+  'https://img.icons8.com/?size=100&id=73&format=png&color=000000';
+export const financesUri =
+  'https://img.icons8.com/?size=100&id=484&format=png&color=000000';
--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -2,46 +2,28 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import TabMainScreen from './TabMainscreen';
 import TabMyFinances from './TabMyFinances';
 import {RootTabParamList} from './types';
-import {Image} from 'react-native';
-const homeUri =
-  'https://img.icons8.com/?size=100&id=73&format=png&color=000000';
-const financesUri =
-  'https://img.icons8.com/?size=100&id=484&format=png&color=000000';
+import TabBarIcon from '../components/tabBarIcon';
+import {financesUri, homeUri} from '../constants/uri';
 
 const Router = () => {
   const {Navigator, Screen} = createBottomTabNavigator<RootTabParamList>();
   return (
-    <Navigator screenOptions={{headerShown: false}}>
+    <Navigator
+      screenOptions={{headerShown: false, tabBarActiveTintColor: '#c374a4'}}>
       <Screen
         options={{
-          tabBarIcon: ({focused, color, size}) => (
-            <Image
-              style={{
-                width: size,
-                height: size,
-                tintColor: focused ? '#c374a4' : 'black',
-              }}
-              source={{uri: homeUri}}
-            />
+          tabBarIcon: ({focused}) => (
+            <TabBarIcon focused={focused} uri={homeUri} />
           ),
-          tabBarActiveTintColor: '#c374a4',
         }}
         name="Main"
         component={TabMainScreen}
       />
       <Screen
         options={{
-          tabBarIcon: ({focused, color, size}) => (
-            <Image
-              style={{
-                width: size,
-                height: size,
-                tintColor: focused ? '#c374a4' : 'black',
-              }}
-              source={{uri: financesUri}}
-            />
+          tabBarIcon: ({focused}) => (
+            <TabBarIcon focused={focused} uri={financesUri} />
           ),
-          tabBarActiveTintColor: '#c374a4',
         }}
         name="Finances"
         component={TabMyFinances}
